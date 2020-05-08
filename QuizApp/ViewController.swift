fe2dece4e5543f8dc4db06784473c8c16f9a25a1
Karlo Lochert <karlo.lochert@gmail.com>
2020-05-08 22:22:43 +0200

    0. zadaca

--- QuizApp/ViewController.swift
+++ QuizApp/ViewController.swift
@@ -15,6 +15,25 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBOutlet weak var usernameField: UITextField!
+    
+    @IBOutlet weak var passwordField: UITextField!
+    @IBAction func SignIn(_ sender: UIButton) {
+        if (sender.tag == 0){
+            if (usernameField.text == "Termitx" && passwordField.text == "Lozinka"){
+                print ("Successful sign in!")
+            }else{
+                print("Wrong username or password!")
+            }
+        }
+    }
+    @IBAction func Clear(_ sender: UIButton) {
+        if (sender.tag == 1){
+            print("Fields cleared")
+            usernameField.text = ""
+            passwordField.text = ""
+        }
 
+    }
 }
 
