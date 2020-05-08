496f123839a298359ec4d564d9444431c392ab84
Karlo Lochert <karlo.lochert@gmail.com>
2020-05-08 21:42:45 +0200

    initial commit

---
+++ QuizApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuizApp
+//
+//  Created by five on 08/05/2020.
+//  Copyright © 2020 karlo lochert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
